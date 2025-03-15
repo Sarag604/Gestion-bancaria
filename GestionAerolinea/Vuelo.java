7a3e597f08de87b4823c3618fdedf09f218715e3
SaraG <saragranda59@gmail.com>
2025-03-15 10:50:41 -0500

    Adelanto de la clase vuelo

--- GestionAerolinea/Vuelo.java
+++ GestionAerolinea/Vuelo.java
@@ -5,5 +5,19 @@ public class Vuelo {
     private String origen;
     private String destino;
     private Aeronave aeronave;
+    private Pasajeros pasajeros;
+
+    public Vuelo(String codigoVuelo, String origen, String destino, Aeronave aeronave, Pasajeros pasajeros) {
+        this.codigoVuelo = codigoVuelo;
+        this.origen = origen;
+        this.destino = destino;
+        this.aeronave = aeronave;
+        this.pasajeros = pasajeros;
+    }
+
+    // metodos para modiicar info
+    public void setCodigoVuelo(String nuevoCodigoVuelo) {
+        this.codigoVuelo = nuevoCodigoVuelo;
+    }
 
 }
