64886051e72f5b88d81f070dc0ca3489b7cb3d04
SaraG <saragranda59@gmail.com>
2025-03-17 11:09:52 -0500

    Se termina la clase vuelo con todas las clases implementadas

--- GestionAerolinea/Vuelo.java
+++ GestionAerolinea/Vuelo.java
@@ -6,6 +6,7 @@ public class Vuelo {
     private String destino;
     private Aeronave aeronave;
     private Pasajeros pasajeros;
+    private int pasajerosActuales = 0;
 
     public Vuelo(String codigoVuelo, String origen, String destino, Aeronave aeronave, Pasajeros pasajeros) {
         this.codigoVuelo = codigoVuelo;
@@ -20,4 +21,48 @@ public class Vuelo {
         this.codigoVuelo = nuevoCodigoVuelo;
     }
 
+    public void setOrigen(String origenNuevo) {
+        this.origen = origenNuevo;
+    }
+
+    public void setDestino(String destinoNuevo) {
+        this.destino = destinoNuevo;
+    }
+
+    // metodos para obtener info
+    public String getCodigoVuelo() {
+        return codigoVuelo;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    // metodo para agregar pasajeros
+    public void setAgregaPasajeros(int agregaPasajeros) {
+        int capacidadMaxima = aeronave.getCapacidad();
+        if (agregaPasajeros + pasajerosActuales > capacidadMaxima) {
+            System.out.println("La aeronave ya llego a su capacidad maxima de pasajeros ");
+        } else {
+            pasajerosActuales += agregaPasajeros;
+            System.out.println("Bienvenido a la aeronave " + aeronave.getModelo() + ". Se han agregado "
+                    + agregaPasajeros + " pasajeros.");
+        }
+    }
+
+    // Método para asignar una aeronave
+    public void asignarAeronave(Aeronave aeronave) {
+        this.aeronave = aeronave;
+    }
+
+    // metodo para mostrar informacion
+    public String mostrarInfo() {
+        return pasajeros.mostrarInfo() + "Su codigo de vuelo es : " + codigoVuelo + ".Su origen de vuelo es " + origen
+                + " y su destino de vuelo es " + destino +
+                " La aeronave para su vuelo es " + aeronave.getModelo();
+    }
 }
